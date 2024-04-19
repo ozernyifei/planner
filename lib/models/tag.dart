5fbe882a303f8ed5fa5d93043e53af6753189155
lebowskd <ozernyifei@outlook.com>
2024-04-19 13:37:00 +0300

    fill tag

---
+++ lib/models/tag.dart
@@ -0,0 +1,26 @@
+class Tag {
+    Tag({
+    required this.id,
+    required this.title,
+    required this.color,   
+  });
+
+  factory Tag.fromMap(Map<String, dynamic> map) {
+    return Tag(
+      id: map['id'] ?? 0,
+      title: map['title'],
+      color: map['color'],
+    );
+  }
+  int id;
+  String title;
+  String color;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'color':color,
+    };
+  }
+}
