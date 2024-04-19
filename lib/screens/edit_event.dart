cc4d15acd8730ce40bd20ab37794f50e426d9912
lebowskd <ozernyifei@outlook.com>
2024-04-19 13:51:50 +0300

    rename model to models

--- lib/screens/edit_event.dart
+++ lib/screens/edit_event.dart
@@ -1,13 +1,13 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
-// Примеры импортов для работы с базой данных и моделями
 import 'package:sqflite/sqflite.dart';
-import '../models/task.dart'; // Модель задачи
+import '../models/task.dart'; 
 
-class EditTaskScreen extends StatefulWidget {
-  final Task? task; // Передаваемая задача (null - новая задача)
-
-  const EditTaskScreen({Key? key, this.task}) : super(key: key);
+class EditTaskScreen extends StatefulWidget { 
+  const EditTaskScreen({super.key, this.task});
+  final Task? task;
 
   @override
   _EditTaskScreenState createState() => _EditTaskScreenState();
@@ -25,13 +25,13 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
     super.initState();
     if (widget.task != null) {
       _titleController.text = widget.task!.title;
-      _descriptionController.text = widget.task!.description;
+      _descriptionController.text = widget.task!.description!;
       _dueDateController.text = widget.task!.dueDate.toIso8601String();
     }
   }
 
   // Сохранение задачи
-  void _saveTask() async {
+  Future<void> _saveTask() async {
     // ... логика сохранения задачи в базе данных
   }
 
@@ -39,22 +39,22 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Создать/Редактировать задачу'),
+        title: const Text('Создать/Редактировать задачу'),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               // Поле ввода для названия задачи
               TextField(
                 controller: _titleController,
-                decoration: InputDecoration(labelText: 'Название'),
+                decoration: const InputDecoration(labelText: 'Название'),
               ),
               // Поле ввода для описания задачи
               TextField(
                 controller: _descriptionController,
-                decoration: InputDecoration(labelText: 'Описание'),
+                decoration: const InputDecoration(labelText: 'Описание'),
                 maxLines: 5,
               ),
               // Поле ввода для даты выполнения
@@ -63,7 +63,7 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
                 readOnly: true, // Дата не редактируется вручную
                 onTap: () async {
                   // Открытие календаря для выбора даты
-                  DateTime? pickedDate = await showDatePicker(
+                  final pickedDate = await showDatePicker(
                     context: context,
                     initialDate: DateTime.now(),
                     firstDate: DateTime(2000),
@@ -75,12 +75,12 @@ class _EditTaskScreenState extends State<EditTaskScreen> {
                     });
                   }
                 },
-                decoration: InputDecoration(labelText: 'Дата выполнения'),
+                decoration: const InputDecoration(labelText: 'Дата выполнения'),
               ),
               // Кнопка сохранения задачи
               ElevatedButton(
                 onPressed: _saveTask,
-                child: Text('Сохранить'),
+                child: const Text('Сохранить'),
               ),
             ],
           ),
