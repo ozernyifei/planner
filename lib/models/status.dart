f7b852d586d5f40f810722d7cc34247fbf9a9309
lebowskd <ozernyifei@outlook.com>
2024-04-19 13:46:42 +0300

    fill status

---
+++ lib/models/status.dart
@@ -0,0 +1,26 @@
+class Status {
+  Status({
+    required this.id,
+    required this.title,
+    required this.description   
+  });
+
+  factory Status.fromMap(Map<String, dynamic> map) {
+    return Status(
+      id: map['id'] ?? 0,
+      title: map['title'],
+      description: map['description'],
+    );
+  }
+  int id;
+  String title;
+  String description;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+    };
+  }
+}
