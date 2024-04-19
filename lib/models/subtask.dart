53a54fac1092d6439d7109023a5a54304c3cf9f8
lebowskd <ozernyifei@outlook.com>
2024-04-19 13:36:45 +0300

    fill subtask

---
+++ lib/models/subtask.dart
@@ -0,0 +1,31 @@
+class Subtask {
+
+  Subtask({
+    required this.id,
+    required this.title,
+    this.description = '',
+    required this.taskId,
+  });
+
+  factory Subtask.fromMap(Map<String, dynamic> map) {
+    return Subtask(
+      id: map['id'] ?? 0,
+      title: map['title'],
+      description: map['description'] ?? '',
+      taskId: map['taskId'],
+    );
+  }
+  int id;
+  String title;
+  String? description;
+  int taskId;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'taskId': taskId,
+    };
+  }
+}
