00197fab7f22b21f85e08da0e07d4c9082afe6b7
lebowskd <ozernyifei@outlook.com>
2024-04-19 13:05:38 +0300

    clean database_helper from models

--- lib/classes/database_helper.dart
+++ lib/classes/database_helper.dart
@@ -1,77 +1,5 @@
 import 'package:sqflite/sqflite.dart';
 
-class Task {
-
-  Task({
-    required this.id,
-    required this.title,
-    this.description = '',
-    required this.dueDate,
-    required this.priorityId,
-    required this.statusId,
-    required this.tagId,
-    required this.reminderId,
-  });
-
-  factory Task.fromMap(Map<String, dynamic> map) {
-    return Task(
-      id: map['id'] ?? 0,
-      title: map['title'],
-      description: map['description'] ?? '',
-      dueDate: DateTime.parse(map['dueDate']),
-      priorityId: map['priorityId'],
-      statusId: map['statusId'],
-      tagId: map['tagId'],
-      reminderId: map['reminderId'],
-    );
-  }
-  int id;
-  String title;
-  String description;
-  DateTime dueDate;
-  int priorityId;
-  int statusId;
-  int tagId;
-  int reminderId;
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-      'description': description,
-      'dueDate': dueDate.toIso8601String(),
-      'priorityId': priorityId,
-      'statusId': statusId,
-      'tagId': tagId,
-      'reminderId': reminderId,
-    };
-  }
-}
-
-class Status {
-
-}
-
-class Tag {
-  
-}
-
-class Priority {
-  
-}
-
-class Subtask {
-  
-}
-
-class Period {
-  
-}
-
-class TaskTag {
-  
-}
-
 class DatabaseHelper {
   static const String _databaseName = 'task_manager.db';
 
@@ -93,7 +21,7 @@ class DatabaseHelper {
         priorityId INTEGER NOT NULL REFERENCES priority(id),
         statusId INTEGER NOT NULL REFERENCES status(id),
         tagId INTEGER NOT NULL REFERENCES tag(id),
-        reminderId INTEGER NOT NULL
+        reminderId INTEGER REFERENCES reminder(id)
       )
     ''');
     // Добавить методы для создания остальных таблиц
